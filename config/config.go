a62f3e0514048dea11cb89226ea2768ae9e2cd5b
yuwei1 <yuweijiubayiqi@163.com>
2020-03-01 19:45:28 +0800

    update

--- config/config.go
+++ config/config.go
@@ -39,7 +39,7 @@ func Init() {
 	//获取当前绝对路径
 	appPath, _ := filepath.Abs(filepath.Dir(filepath.Join("."+sp, sp)))
 	// 获得conf的路径
-	pt := filepath.Join(appPath, "./../srv_conf")
+	pt := filepath.Join(appPath, "./srv_conf")
 	//设置工作目录
 	fmt.Println(pt)
 	// os.Chdir(filepath.Join(pt, "."))
